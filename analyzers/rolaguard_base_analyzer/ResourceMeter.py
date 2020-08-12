3c5cf796a522dcf226a8ca2a26c8235de786d0a9
cyones <cyones77@gmail.com>
2020-08-12 14:51:37 -0300

    Added comments, fix division by zero problem and use date to calculate time diff.

--- analyzers/rolaguard_base_analyzer/ResourceMeter.py
+++ analyzers/rolaguard_base_analyzer/ResourceMeter.py
@@ -3,7 +3,9 @@ from datetime import date
 
 
 class ResourceMeter():
-    maw = 0.9 # Moving average weight
+    # Moving average weight, must be between 0 and 1.
+    # The greater the value is, the longer is the period averaged to calculate stats.
+    maw = 0.9
 
     def __init__(self):
         self.device_stats = {}
@@ -11,6 +13,14 @@ class ResourceMeter():
         self.last_gc = date.today()
 
     def __call__(self, asset, packet):
+        """
+        Main function of the ResourceMeter. It calculates some stats for the assets
+        and update the corresponding fields in the DB object (it doesn't commit the
+        changes in the DB).
+        Parameters:
+        - asset: a gateway or device object
+        - packet: the current packet processed
+        """
         if not asset or not packet: return
         if not packet.f_count or not packet.date: return
         if packet.m_type in ["JoinRequest", "JoinAccept"]: return
@@ -19,7 +29,7 @@ class ResourceMeter():
 
         if type(asset) == Device:
             if asset.id in self.device_stats:
-                self.measure_device_stats(asset, packet)
+                self.device_resource_usage(asset, packet)
                 if packet.uplink:
                     self.device_stats[asset.id]["last_fcount"] = packet.f_count
                 else:
@@ -34,15 +44,14 @@ class ResourceMeter():
                 }
         if type(asset) == Gateway:
             if asset.id in self.gateway_stats:
-                self.measure_gateway_stats(asset, packet)
+                self.gateway_resource_usage(asset, packet)
             self.gateway_stats[asset.id] = {
                 "last_fcount" : packet.f_count,
-                "last_tmst" : packet.tmst,
                 "last_date" : packet.date
             }
 
 
-    def measure_device_stats(self, device, packet):
+    def device_resource_usage(self, device, packet):
         if packet.uplink and packet.f_count == self.device_stats[device.id]["last_fcount"]: 
             return # Repeated uplink packet
         if not packet.uplink and packet.f_count == self.device_stats[device.id]["last_fcount_down"]: 
@@ -59,22 +68,24 @@ class ResourceMeter():
             count_diff = int(packet.f_count - last_fcount) % (2**16)
 
             # The counter changed a lot, probably the session was restarted
-            if count_diff > 64 or count_diff < 0:
+            if count_diff > 64 or count_diff < 1:
                 del self.device_stats[device.id]
                 return
 
+            # Update activity_freq (which is the time between packets)
             time_diff = (packet.date - self.device_stats[device.id]["last_date"]).seconds / count_diff
-
             if device.activity_freq:
                 device.activity_freq = self.maw * device.activity_freq + (1-self.maw) * time_diff
             else:
                 device.activity_freq = time_diff
 
+            # Update the mean number of packets lost
             if device.npackets_lost:
                 device.npackets_lost = self.maw * device.npackets_lost + (1-self.maw) * (count_diff-1)
             else:
                 device.npackets_lost = count_diff-1
-            
+
+            # Update the max (from all the gateways) rssi of the device
             if packet.gateway in self.device_stats[device.id]["rssi"]:
                 self.device_stats[device.id]["rssi"][packet.gateway] = \
                     self.maw *  self.device_stats[device.id]["rssi"][packet.gateway] + \
@@ -84,25 +95,33 @@ class ResourceMeter():
             device.max_rssi = max(self.device_stats[device.id]["rssi"].values())
 
 
-    def measure_gateway_stats(self, gateway, packet):
+    def gateway_resource_usage(self, gateway, packet):
         gateway.npackets_up += 1 if packet.uplink else 0
         gateway.npackets_down += 1 if not packet.uplink else 0
         gateway.last_activity = packet.date
         gateway.connected = True
 
-        last_tmst = self.gateway_stats[gateway.id]["last_tmst"]
-        time_diff = (int(packet.tmst - last_tmst) % (2**32)) / 1e9
-
+        # Update activity_freq (which is the time between packets)
+        time_diff = (packet.date - self.gateway_stats[gateway.id]["last_date"]).seconds
         if gateway.activity_freq:
             gateway.activity_freq = self.maw * gateway.activity_freq + (1-self.maw) * time_diff
         else:
             gateway.activity_freq = time_diff
 
     def gc(self, today):
+        """
+        This function deletes the stats from assets that hasn't send a packet in
+        the last 30 days. This data is no longer used since the assets are probably 
+        disconnected. This way we prevent filling the RAM memory of the node. 
+        The function doesn't do anything if the last gc was run less than an hour ago,
+        therefore, it can be called for each packet without a hit in performance.
+        Parameter:
+        - today: date of the packet thats being procesed.
+        """
         if (date.today() - self.last_gc).seconds > 3600:
-            todel = [k for k, v in self.device_stats.items() if (today - v['last_date']).days > 7]
+            todel = [k for k, v in self.device_stats.items() if (today - v['last_date']).days > 30]
             for k in todel: del self.device_stats[k]
-            todel = [k for k, v in self.gateway_stats.items() if (today - v['last_date']).days > 7]
+            todel = [k for k, v in self.gateway_stats.items() if (today - v['last_date']).days > 30]
             for k in todel: del self.gateway_stats[k]
             self.last_gc = date.today()
             
