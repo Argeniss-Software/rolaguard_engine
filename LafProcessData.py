85e79a8f203566c723b03965f76933f73f004691
cyones <cyones77@gmail.com>
2020-11-12 17:57:36 -0300

    small fix

--- LafProcessData.py
+++ LafProcessData.py
@@ -184,7 +184,7 @@ def processData():
                 except Exception as exc:
                     rollback()
                     log.error("Error trying to commit after packet processing finish: {0}".format(exc))
-                inner_chrono.stop()
+                inner_chrono.lap()
                 
             if options.report_stats:
                 chrono.start("stats")
